879a9f4db5e3e4ecaedc2373dfe7ce39543b436d
Morgado <morgadodesarrollador@gmail.com>
2021-10-21 10:25:30 +0200

    proyectos pwa

---
+++ PWA/02-service-worker/js/app.js
@@ -0,0 +1,9 @@
+console.log('sw');
+if ('serviceWorker' in navigator) {
+    console.log ('podemos usarlo');
+}
+// confitmar si podemos usar el ServiceWorker
+if (navigator.serviceWorker){
+    console.log ('podemos usarlo');
+    navigator.serviceWorker.register('/sw.js')
+}
