4cd39db920d4629ec1d2b509e96ccafa94b1ffb8
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2021-11-30 14:29:45 +0100

    JavaScript ES6-. ArrowFunctions

--- HTML/3-RWD/2021/maq2-MFirst/js/app copy.js
+++ HTML/3-RWD/2021/maq2-MFirst/js/app copy.js
@@ -1,33 +1,36 @@
-
 console.log ('Cargando app.js');
 const iva = 0.16;
 
 let pcoste = 2000;
-calcularPVP(pcoste, iva);
-console.log ('---------- Producto 1 -------');
-console.log ('Precio de Coste = ', pcoste, ' €');
-console.log (' el iva es = ', iva * 100, '%', '-->', pcoste*iva);
-console.log ('PVP = ', pcoste * (1 + iva), ' €');
-console.log ('---------- Producto 2 -------');
-pcoste = 1000;
-console.log ('Precio de Coste = ', pcoste, ' €');
-console.log ('el iva es= ', iva * 100, '%', '-->', pcoste*iva);
-console.log ('PVP = ', pcoste * (1 + iva), ' €');
-console.log ('---------- Producto 3 -------');
-pcoste = 500;
-console.log ('Precio de Coste = ', pcoste, ' €');
-console.log ('iva = ', iva * 100, '%', '-->', pcoste*iva);
-console.log ('PVP = ', pcoste * (1 + iva), ' €');
-
-console.log ('---------- Producto 4 -------');
-pcoste = 100;
-console.log ('Precio de Coste = ', pcoste, ' €');
-console.log ('iva = ', iva * 100, '%', '-->', pcoste*iva);
-console.log ('PVP = ', pcoste * (1 + iva), ' €');
-
-function calcularPVP(precio, impuesto){
+//js --> JavaScript ES6
+// un varible que contiene una función
+// la variable se llama calcularPVP
+const calcularPVP2 = function (precio, impuesto){
     console.log ('Precio de Coste = ', precio, ' €');
     console.log ('iva = ', impuesto * 100, '%', '-->', precio*impuesto);
     console.log ('PVP = ', precio * (1 + impuesto), ' €');
-
+    console.log('--------------------------');
 }
+// la función anterior se reescribe en notación Arrow Function
+const calcularPVP = (precio, impuesto) => {
+    console.log ('Precio de Coste = ', precio, ' €');
+    console.log ('iva = ', impuesto * 100, '%', '-->', precio*impuesto);
+    console.log ('PVP = ', precio * (1 + impuesto), ' €');
+    return precio * (1 + impuesto);
+}
+
+//llamadas a la función
+let ptotal = calcularPVP(pcoste, iva);
+console.log (ptotal);
+
+
+
+
+// Función externa --> la función se llama calcularPVP1
+function calcularPVP1(precio, impuesto){
+    console.log ('Precio de Coste = ', precio, ' €');
+    console.log ('iva = ', impuesto * 100, '%', '-->', precio*impuesto);
+    console.log ('PVP = ', precio * (1 + impuesto), ' €');
+    console.log('--------------------------');
+}
+
--- HTML/3-RWD/2021/maq2-MFirst/js/app.js
+++ HTML/3-RWD/2021/maq2-MFirst/js/app.js
@@ -1,21 +1,61 @@
-
 console.log ('Cargando app.js');
 const iva = 0.16;
 
 let pcoste = 2000;
-//llamadas a la función
-calcularPVP(pcoste, iva);
-calcularPVP(1000, iva);
-calcularPVP(3000, iva);
-pcoste = 10;
-calcularPVP(pcoste, iva);
 
-
-// declarar la funcion calcularPVP
-function calcularPVP(precio, impuesto){
+const calcularPVP = (precio, impuesto) => {
     console.log ('Precio de Coste = ', precio, ' €');
     console.log ('iva = ', impuesto * 100, '%', '-->', precio*impuesto);
     console.log ('PVP = ', precio * (1 + impuesto), ' €');
-    console.log('--------------------------');
-
+    return precio * (1 + impuesto);
 }
+
+//llamadas a la función
+//let ptotal = calcularPVP(20, iva);
+
+function sumar1 (x,y){
+    // instruccion 1;
+    // instruccion 2;
+    //...
+    //instruccion n;
+    return ( x + y)
+}
+
+/*  solo cuando el cuerpo de la funcion tiene 1 sola instruccion
+const sumar2 = (x, y) => {
+    return x + y;
+}
+*/
+const sumar2 = (x, y) =>  x + y ;
+
+let suma = sumar2( 13, 6);
+console.log(suma);
+
+const cubo1 = ( x ) => {
+    return( x * x * x)
+}
+const cubo = (x) => x*x*x;
+console.log( cubo(5,) );
+
+const potencia = (b, e) => {
+    let res = 1;
+    for(i = 1; i <= e; i++){
+        res = res * b
+    }
+    return res;
+}
+console.log (potencia (3,3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
