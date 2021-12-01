c8908b1f75094bc2368445301886208047457200
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2021-12-01 14:04:25 +0100

    JavaScript ES6-. Carga Dinamica

--- HTML/3-RWD/2021/maq2-MFirst/js/app.js
+++ HTML/3-RWD/2021/maq2-MFirst/js/app.js
@@ -1,68 +1,14 @@
-console.log ('Cargando app.js');
-const iva = 0.16;
 
-let pcoste = 2000;
 
-const calcularPVP = (precio, impuesto) => {
-    console.log ('Precio de Coste = ', precio, ' €');
-    console.log ('iva = ', impuesto * 100, '%', '-->', precio*impuesto);
-    console.log ('PVP = ', precio * (1 + impuesto), ' €');
-    return precio * (1 + impuesto);
+// carga dinámica de pÁGINAS
+function inicio() {
+    console.log('documento app cargado');
+    console.log(document);
+    const vmain = document.querySelector('main');
+    //vmain.innerHTML = '<h1>Hola Caracola</h1>';
+    
+
 }
-
-//llamadas a la función
-//let ptotal = calcularPVP(20, iva);
-
-function sumar1 (x,y){
-    // instruccion 1;
-    // instruccion 2;
-    //...
-    //instruccion n;
-    return ( x + y)
-}
-
-/*  solo cuando el cuerpo de la funcion tiene 1 sola instruccion
-const sumar2 = (x, y) => {
-    return x + y;
-}
-*/
-const sumar2 = (x, y) =>  x + y ;
-
-let suma = sumar2( 13, 6);
-console.log(suma);
-
-const cubo1 = ( x ) => {
-    return( x * x * x)
-}
-const cubo = (x) => x*x*x;
-console.log( cubo(5,) );
-
-const potencia = (b, e) => { // 3 * 3 * 3 * 3 * 3   --> 3 + 3 + 3 + 3 + 3
-    let res = 1;
-    for(i = 1; i <= e; i++){
-        res = res * b
-    }
-    return res;
-}
-//console.log (potencia (3,5));
-
-// sumer los 10 primeros numeros 
-const suma10 = ( n ) => {
-    let stotal = 0;
-    for (i = 0; i <= n ; i ++){
-        stotal = stotal + i;
-    }
-    return stotal;
-}
-console.log(suma10( 2 ));
-console.log(suma10( 5 ));
-console.log(suma10( 100 ));
-
-
-
-
-
-
 
 
 
