cd5892cf5933ac76c645e9fce210bfe4649eb7a6
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-02-09 16:43:46 +0100

    portfolio

--- Angular/2022/Libreria/src/app/app.module.ts
+++ Angular/2022/Libreria/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule } from './estructura/maqueta/footer/footer.module';
+import { HeaderModule } from './estructura/maqueta/header/header.module';
+import { MainModule } from './estructura/maqueta/main/main.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    MainModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
