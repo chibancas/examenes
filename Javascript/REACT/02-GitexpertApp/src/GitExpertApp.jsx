448a3f3f54a678fc158b61882d506c2029adc2c0
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-11-06 23:08:01 +0100

    02-GitExpertApp-Categorias:Listar

--- Javascript/REACT/02-GitexpertApp/src/GitExpertApp.jsx
+++ Javascript/REACT/02-GitexpertApp/src/GitExpertApp.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react"
 
 const GitExpertApp = () => {
-  return (
-    <>
-        <h1>GitExpertApp</h1>
-    </>
-  )
+    
+    //categorias = ['One Punch']
+    const [ categorias, setCategorias ] = useState ( ['One Punch', 'Dragon Ball'] );
+    console.log (categorias);
+    return (
+        <>
+            {/* titulo */}
+            <h1>GitExpertApp</h1>
+
+
+            {/* buscador de giphy */}
+
+            {/* listado de tarjetas con cada giphy */}
+            <ol>
+                { 
+                    categorias.map ( (categoria) => {
+                        return <li key={categoria}> { categoria } </li>
+                    })
+                }
+            </ol>
+                {/* Giph Item */}
+        </>
+    )
 }
 
 export default GitExpertApp
